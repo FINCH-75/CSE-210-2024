f9c7149a41ff847bd7d2ebdbe6a302cf7bae4839
Finch-75 <williamchadfinch@gmail.com>
2024-01-10 09:51:33 -0700

    Week  Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello my freind!");
+
+        Console.WriteLine("What is your first name?");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}!");
+        
+
     }
 }
