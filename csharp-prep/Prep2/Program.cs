752ef7b8105cb55c2f2d0853be4c471f1d0e38c4
Finch-75 <williamchadfinch@gmail.com>
2024-01-10 10:56:43 -0700

    week2 prove2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your final score?");
+        string userScore = Console.ReadLine();
+        int grade = int.Parse(userScore);
+        
+        
+        
+
+        if (grade >= 90)
+        {
+             string textGrade = grade.ToString();
+             textGrade = "A";
+             Console.WriteLine($"You got a {textGrade}");
+             Console.WriteLine($"Congrats you passed!");
+             
+        }
+        else if (grade >= 80)
+        {
+            string textGrade = grade.ToString();
+            textGrade = "B";
+            Console.WriteLine($"You got a {textGrade}");
+            Console.WriteLine($"Congrats you passed!");
+        }
+        else if (grade >= 70)
+        {
+            string textGrade = grade.ToString();
+            textGrade = "C";
+            Console.WriteLine($"You got a {textGrade}");
+            Console.WriteLine($"Congrats you passed!");
+
+        }
+        else if (grade >= 60)
+        {
+        string textGrade = grade.ToString();
+        textGrade = "D";
+        Console.WriteLine($"You got a {textGrade}");
+        Console.WriteLine($"Sorry you did not pass.");
+
+        }
+        else 
+        {
+           string textGrade = grade.ToString();
+           textGrade = "F";
+           Console.WriteLine($"You got a {textGrade}");
+           Console.WriteLine($"Sorry you did not pass."); 
+
+        }
+
+        
     }
 }
