16acca7adad83d1fa0fe1ebdecba5f15e5bc43d1
Finch-75 <williamchadfinch@gmail.com>
2024-01-17 19:25:20 -0700

    week 2 learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Power Production Specialist 2";
+        job1._company = "Tucson Electric Power";
+        job1._startDate = 2015;
+        job1._endDate = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistant Control Room Operator";
+        job2._company = "Tucson Electric Power";
+        job2._startDate = 2023;
+        job2._endDate = 2024;
+
+
+        Resume myResume = new Resume();
+        myResume._name = "William Chad Finch";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
